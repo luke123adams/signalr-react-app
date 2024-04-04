d34fc3b8c226be2da80ec866fb0d71c66dd5e265
luke123adams <lxa207@gmail.com>
2024-04-04 13:04:11 +0100

    completed tutorial

--- SignalRWebpack/Program.cs
+++ SignalRWebpack/Program.cs
@@ -1,6 +1,13 @@
+using SignalRWebpack.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/hub");
 
 app.Run();
